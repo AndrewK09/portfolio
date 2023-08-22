692285919b042f6923cf92c363bcc869bee70b15
Andrew <Andrewkn09@Gmail.com>
2023-08-22 00:03:50 -0400

    Setup initial page

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+import About from "./(about)/page"
+
+const Portfolio = () => {
+
+  return (
+    <div className="w-full mx-auto max-w-screen-lg pt-52 py-12 px-10 font-sans">
+
+
+      <header className=''>
+        <h1 className='text-6xl font-bold sm:text-7xl'>Andrew Kan</h1>
+        <h2 className='text-2xl sm:text-xl font-medium mt-3'>Full Stack Software Engineer</h2>
+        <p className='text-lg mt-6 max-w-xs'>I build accessible, inclusive products and digital experiences for the web.</p>
+      </header>
+
+
+
+      <main className='pt-24'>
+        <About />
+      </main>
+
+
+
+    </div>
+  )
+}
+
+export default Portfolio
