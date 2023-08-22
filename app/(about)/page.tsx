a55690db8b1abf2827285cf6b8c16c00de8d06b2
Andrew <Andrewkn09@Gmail.com>
2023-08-22 00:03:20 -0400

    Start of about me page

---
+++ app/(about)/page.tsx
@@ -0,0 +1,18 @@
+const About = () => {
+
+  return (<section>
+    <h1 className="text-3xl font-medium">About me</h1>
+    <p className='mt-4'>
+      Back in 2012, I decided to try my hand at creating custom Tumblr themes and tumbled head first into the rabbit hole of coding and web development. Fast-forward to today, and I’ve had the privilege of building software for an advertising agency, a start-up, a student-led design studio, and a huge corporation.
+    </p>
+    <p className='mt-4'>
+      My main focus these days is building products and leading projects for our clients at Upstatement. In my free time I've also released an online video course that covers everything you need to know to build a web app with the Spotify API.
+    </p>
+    <p className='mt-4'>
+      Back in 2012, I decided to try my hand at creating custom Tumblr themes and tumbled head first into the rabbit hole of coding and web development. Fast-forward to today, and I’ve had the privilege of building software for an advertising agency, a start-up, a student-led design studio, and a huge corporation.
+    </p>
+  </section>)
+
+}
+
+export default About
