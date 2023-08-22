197f81ab5b2e1b5298d71a1ae9c9f2ff6b0616f4
Andrew <Andrewkn09@Gmail.com>
2023-08-22 13:18:36 -0400

    Use Projects component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
-import Experiences from "@/components/Experiences"    
+import Experiences from "@/components/Experiences"
 import About from "@/components/About"
+import Projects from "@/components/Projects"
 
 const Portfolio = () => {
 
   return (
-    <div className="w-full mx-auto max-w-screen-lg pt-52 py-12 px-10 font-sans">
+    <div className="w-full mx-auto max-w-screen-lg py-12 px-10 font-sans bg-slate-800">
 
 
-      <header className=''>
+      <header className='py-52'>
         <h1 className='text-6xl font-bold sm:text-7xl'>Andrew Kan</h1>
         <h2 className='text-2xl sm:text-xl font-medium mt-3'>Full Stack Software Engineer</h2>
         <p className='text-lg mt-6 max-w-xs'>I build accessible, inclusive products and digital experiences for the web.</p>
@@ -18,6 +19,7 @@ const Portfolio = () => {
       <main className='pt-24'>
         <About />
         <Experiences />
+        <Projects />
       </main>
 
 
