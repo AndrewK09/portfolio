e4199de4ed0493afbe37d68e6f1fea5930d3b7fc
Andrew <Andrewkn09@Gmail.com>
2023-08-24 21:56:10 -0400

    Add styling for main body

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,21 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-
+import './globals.css';
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Andrew Kan',
   description: 'Andrew Kan portfolio',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-slate-700 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900">
+        {children}
+      </body>
     </html>
-  )
+  );
 }
