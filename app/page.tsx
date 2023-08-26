76be1de85142209c14dbe9be6cdc489ed7ee8ff9
Andrew <Andrewkn09@Gmail.com>
2023-08-26 18:13:38 -0400

    Fix css responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -11,16 +11,16 @@ const SocialIconWrap = ({ url }: { url: string }) => {
 
 const Portfolio = () => {
   return (
-    <div className=" xl:px-18 mx-auto w-full max-w-screen-xl gap-x-20 px-10 lg:px-16 xl:flex xl:max-w-screen-2xl xl:justify-between">
-      <header className="py-52 lg:top-0 xl:sticky xl:flex xl:max-h-screen xl:min-w-fit xl:flex-col xl:justify-between xl:py-24 2xl:w-2/5">
+    <div className=" lg:px-18 mx-auto w-full max-w-screen-xl px-10 lg:flex lg:max-w-screen-xl lg:justify-between lg:gap-x-28 lg:px-16">
+      <header className="py-52 lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:min-w-fit lg:flex-col lg:justify-between lg:py-24 lg:pb-20 2xl:w-2/5">
         <div>
-          <h1 className="text-6xl font-bold text-slate-100 sm:text-7xl md:text-8xl xl:text-6xl ">
+          <h1 className="text-6xl font-bold text-slate-100 sm:text-6xl md:text-7xl lg:text-6xl">
             Andrew Kan
           </h1>
-          <h2 className="mt-3 text-xl font-medium text-slate-300 sm:text-2xl md:text-3xl xl:text-xl">
+          <h2 className="mt-3 text-2xl font-medium text-slate-300 sm:text-2xl md:text-3xl lg:text-2xl">
             Full Stack Software Engineer
           </h2>
-          <p className="mt-6 max-w-xs text-lg xl:mb-16">
+          <p className="lg: mt-6 text-lg lg:mb-8 lg:max-w-xs">
             I build accessible, inclusive products and digital experiences for
             the web.
           </p>
@@ -28,7 +28,7 @@ const Portfolio = () => {
           <Navbar />
         </div>
 
-        <ul className="-ml-2 mt-4 flex">
+        <ul className="lg: -ml-2 mt-4 flex">
           <li>
             <SocialIconWrap url="https://github.com/AndrewK09" />
           </li>
@@ -39,7 +39,7 @@ const Portfolio = () => {
         </ul>
       </header>
 
-      <main className="w-auto  xl:py-24">
+      <main className="w-auto  lg:py-24">
         <About />
         <Experiences />
         <Projects />
