4756989c8e0b6a8dfa8da326b81398af46e167fc
Andrew <Andrewkn09@Gmail.com>
2023-08-27 15:05:28 -0400

    Add active link state functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,27 +1,58 @@
 'use client';
 
+import { useState } from 'react';
+
 import Link from 'next/link';
 
-const NavbarLink = ({ href, label }: { href: string; label: string }) => {
+enum ActiveLinkType {
+  ABOUT = 'about',
+  EXPERIENCE = 'experience',
+  PROJECT = 'projects',
+  CONTACT = 'contact',
+}
+
+const NavbarLink = ({
+  href,
+  label,
+  activeLink,
+}: {
+  href: string;
+  label: string;
+  activeLink: ActiveLinkType;
+}) => {
   return (
-    <Link href={href} className="group flex items-center py-3">
-      <span className="mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200"></span>
-      <span className="text-xs font-bold uppercase  tracking-widest group-hover:text-slate-200 group-focus-visible:text-slate-200">
+    <Link
+      href={`#${href}`}
+      className={`${
+        activeLink === href ? 'active ' : ''
+      }group flex items-center py-3`}
+    >
+      <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200"></span>
+      <span className="nav-text text-xs font-bold uppercase  tracking-widest group-hover:text-slate-200 group-focus-visible:text-slate-200">
         {label}
       </span>
     </Link>
   );
 };
 
-const navbarLinks = ['about', 'experience', 'projects', 'contact'];
+const navbarLinks = [
+  ActiveLinkType.ABOUT,
+  ActiveLinkType.EXPERIENCE,
+  ActiveLinkType.PROJECT,
+  ActiveLinkType.CONTACT,
+];
 
 const Navbar = () => {
+  const [activeLink, setActiveLink] = useState<ActiveLinkType>(
+    ActiveLinkType.ABOUT
+  );
+
   return (
     <nav className="hidden lg:block">
-      <ul>
+      <ul className="w-max">
         {navbarLinks.map((link) => (
-          <li key={link}>
-            <NavbarLink href={`#${link}`} label={link} />
+          <li key={link} onClick={() => setActiveLink(link)}>
+            <NavbarLink href={link} label={link} activeLink={activeLink} />
           </li>
         ))}
       </ul>
