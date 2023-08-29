666267748553626b67fa0b83e4e574699a335bba
Andrew <Andrewkn09@Gmail.com>
2023-08-29 18:50:35 -0400

    Add technical skills data

--- lib/data.ts
+++ lib/data.ts
@@ -3,12 +3,17 @@ export enum NavLinkType {
   EXPERIENCE = 'experience',
   PROJECT = 'projects',
   CONTACT = 'contact',
+  TECHNOLOGY = 'technology',
 }
 
 export const navLinks = [
   {
     name: NavLinkType.ABOUT,
     href: '#about',
+  },
+  {
+    name: NavLinkType.TECHNOLOGY,
+    href: '#technology',
   },
   {
     name: NavLinkType.EXPERIENCE,
@@ -22,4 +27,60 @@ export const navLinks = [
     name: NavLinkType.CONTACT,
     href: '#contact',
   },
+];
+
+export const FrontendTechnologies = [
+  'JavaScript',
+  'TypeScript',
+  'HTML',
+  'CSS',
+  'React',
+  'Redux',
+  'Next.js',
+  'Tailwind CSS',
+];
+
+export const BackendTechnologies = [
+  'Node.js',
+  'Express',
+  'PostgreSQL',
+  'Sequelize',
+  'SQL',
+  'MySQL',
+  'MongoDB',
+];
+
+export const DevOpsTechnologies = ['Git', 'Heroku', 'Amazon AWS', 'Docker'];
+
+export const TestingTechnologies = ['Mocha', 'Chai', 'Jest'];
+
+export const MainTechnologies = [
+  'JavaScript',
+  'TypeScript',
+  'HTML',
+  'CSS',
+  'React',
+  'Redux',
+  'Node.js',
+  'Express',
+  'PostgreSQL',
+  'Sequelize',
+  'Git',
+  'REST API',
+];
+
+export const OtherTechnologies = [
+  'Next.js',
+  'Tailwind CSS',
+  'SQL',
+  'MySQL',
+  'MongoDB',
+  'VMware vCenter',
+  'VMware NSX',
+  'Heroku',
+  'Amazon AWS',
+  'Docker',
+  'Mocha',
+  'Chai',
+  'Socket.io',
 ];
