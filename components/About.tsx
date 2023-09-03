02bda8c269b6dd112c28008cd3601453b544f092
Andrew <Andrewkn09@Gmail.com>
2023-09-04 06:25:12 +0800

    Change About paragraphs

--- components/About.tsx
+++ components/About.tsx
@@ -10,18 +10,18 @@ const About = () => {
     <section id="about" ref={ref}>
       <h1 className="head_text">ABOUT</h1>
       <p className="mb-4">
-        {`Back in 2012, I decided to try my hand at creating custom Tumblr themes
-        and tumbled head first into the rabbit hole of coding and web
-        development. Fast-forward to today, and I've had the privilege of
-        building software for an advertising agency, a start-up, a student-led
-        design studio, and a huge corporation.`}
+        {`Back in 2019, I had started learning JavaScript, HTML and CSS whenever I had some free time. 
+        I ended up enjoying coding and web development so much that I decided to pursue programming full-time. 
+        I have been a professional software engineer for over 2 years now and had the opportunity to learn a variety of 
+        technologies, both frontend and backend over the past few years. `}
       </p>
-      <p className="mb-4">
-        {`My main focus these days is building products and leading projects for
-        our clients at Upstatement. In my free time I've also released an online
-        video course that covers everything you need to know to build a web app
-        with the Spotify API.`}
+
+      <p>
+        My goal is to build highly performant applications that solve real-world
+        problems and provide users with an awesome experience.
       </p>
+
+      <br />
       <p className="mb-4">
         In my free time, I enjoy practicing piano, learning new things, hiking
         in the summer and snowboarding in the winter.
