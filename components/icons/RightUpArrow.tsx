595b6ac4cb55a277725def6b022ac327c19c6843
Andrew <Andrewkn09@Gmail.com>
2023-09-04 14:32:41 +0800

    Move right up arrow svg into its own RightUpArrowIcon component

---
+++ components/icons/RightUpArrow.tsx
@@ -0,0 +1,16 @@
+export const RightUpArrowIcon = ({ className }: { className: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className={className}
+    >
+      <path d="M7 17l9.2-9.2M17 17V7H7" />
+    </svg>
+  );
+};
+
+export default RightUpArrowIcon;
