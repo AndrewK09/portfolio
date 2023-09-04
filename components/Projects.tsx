5208043e2ae52ca8c08e1a1f6542e4fdaabb89d2
Andrew <Andrewkn09@Gmail.com>
2023-09-04 08:21:39 +0800

    Replace dummy projects with project greenfield and minesweeper projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,19 +1,22 @@
 'use client';
 
 import Image, { StaticImageData } from 'next/image';
-import Link from 'next/image';
 
-import project1 from '@/public/test-background.png';
 import { useSectionInView } from '@/lib/hooks';
 import { NavLinkType } from '@/lib/data';
-import ProjectImage from './ProjectImage';
+
+import project1 from '@/public/test-background.png';
+import shoppingPortal from '@/public/shopify.gif';
+import minesweeper from '@/public/minesweeper-lg.png';
 import SocialIconWrap from './SocialIconWrap';
 
 enum Technologies {
   REACT = 'React',
+  REDUX = 'Redux',
   NODE = 'Node',
   EXPRESS = 'Express',
   POSTGRES = 'PostgreSQL',
+  MATERIAL = 'Material UI',
 }
 
 const Project = ({
@@ -32,14 +35,20 @@ const Project = ({
   websiteUrl?: string;
 }) => {
   return (
-    <li className="relative mb-12 grid grid-cols-6">
+    <li className="relative mb-12 grid grid-cols-6 grid-rows-6 ">
       <div className="relative -z-10 col-span-full row-span-full shadow-lg shadow-blue-950">
         <a>
-          <Image src={image.src} alt={image.alt} className="opacity-10" />
+          <Image
+            src={image.src}
+            alt={image.alt}
+            className="opacity-10"
+            objectFit="cover"
+            layout="fill"
+          />
         </a>
       </div>
 
-      <div className="col-span-full row-span-full px-12 py-14 ">
+      <div className="relative col-span-full row-span-full px-12 py-12 ">
         <header className="mb-10 text-xl font-bold text-white ">{title}</header>
 
         <div className="mb-6 rounded-md bg-blue-950 px-5 py-5 text-white/70 shadow-md ">
@@ -73,33 +82,35 @@ const Projects = () => {
     <section id={NavLinkType.PROJECT} ref={ref}>
       <h1 className="head_text p-2 pt-4">PROJECTS</h1>
       <Project
-        title="Test project 1"
-        description="System to showcase the real time use of xAPI and a LRS system to collect, save and use data to analyze and generate report."
-        image={{ src: project1, alt: 'picture' }}
+        title="Project Greenfield - Shopping Portal"
+        description="Project Greenfield was my first group-project, I worked with 2 other developers. It comprises a complete redesign of an outdated client-facing retail web-portal that includes an image gallery, questions and answers, and ratings and reviews. I was in charge of the ratings and reviews portion."
+        image={{ src: shoppingPortal, alt: 'Project-Greenfield' }}
         technologies={[
           Technologies.REACT,
+          Technologies.REDUX,
+          Technologies.MATERIAL,
           Technologies.NODE,
-          Technologies.POSTGRES,
+          Technologies.EXPRESS,
         ]}
-        githubUrl="test/test"
-        websiteUrl="test/test"
+        githubUrl="https://github.com/hrnyc23-vibranium/project-greenfield/tree/master"
       />
 
       <Project
-        title="Test project 2"
-        description="System to showcase the real time use of xAPI and a LRS system to collect, save and use data to analyze and generate report."
-        image={{ src: project1, alt: 'picture' }}
+        title="Minesweeper Clone"
+        description="Minesweeper is a single page web application that mimicks the original Minesweeper that comes with Windows OS. This was the very first web application I built a month a half into coding bootcamp. We were instructed to spend a day on the MVP and see how far we get, but I enjoyed working on it so much I spent a few more days on it."
+        image={{ src: minesweeper, alt: 'Minesweeper' }}
         technologies={[
           Technologies.REACT,
+          Technologies.REDUX,
           Technologies.NODE,
-          Technologies.POSTGRES,
+          Technologies.EXPRESS,
         ]}
-        githubUrl="test/test"
+        githubUrl="minesweeper"
       />
 
       <Project
         title="Test project 3"
-        description="System to showcase the real time use of xAPI and a LRS system to collect, save and use data to analyze and generate report."
+        description="Minesweeper is a single page web application that mimicks the original Minesweeper that comes with Windows OS. This was the very first web application I built a month a half into coding bootcamp. We were instructed to spend a day on the MVP and see how far we get, but I enjoyed working on this application so much I continued to work on it for a few more days."
         image={{ src: project1, alt: 'picture' }}
         technologies={[
           Technologies.REACT,
