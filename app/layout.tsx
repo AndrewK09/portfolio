279a07581a5214caef7682e7987d1d25bbca1edd
Andrew <Andrewkn09@Gmail.com>
2023-09-21 08:48:38 -0400

    Add Toaster to top right position

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import ActiveSectionContextProvider from '@/context/active-section-context';
 import './globals.css';
 import type { Metadata } from 'next';
+import { Toaster } from 'react-hot-toast';
 
 export const metadata: Metadata = {
   title: 'Andrew Kan',
@@ -15,7 +16,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body className="bg-primary-navy selection:bg-secondary-green selection:text-secondary-navy leading-relaxed text-slate-400 antialiased">
-        <ActiveSectionContextProvider>{children}</ActiveSectionContextProvider>
+        <ActiveSectionContextProvider>
+          <Toaster position="top-right" />
+          {children}
+        </ActiveSectionContextProvider>
       </body>
     </html>
   );
