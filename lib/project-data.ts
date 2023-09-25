e83a177be0e1da28c311b7879d0eeec808053a1b
Andrew <Andrewkn09@Gmail.com>
2023-09-25 09:50:25 -0400

    Update project information

--- lib/project-data.ts
+++ lib/project-data.ts
@@ -24,7 +24,7 @@ interface Project {
 const greenfield = {
   title: 'Project Greenfield - Shopping Portal',
   description:
-    'Project Greenfield was my first group-project, I worked with 2 other developers. It comprises a complete redesign of an outdated client-facing retail web-portal that includes an image gallery, questions and answers, and ratings and reviews. I was in charge of the ratings and reviews portion.',
+    'Project Greenfield comprises a complete redesign of an outdated client-facing retail web-portal that includes an image gallery, questions and answers, and ratings and reviews section. I worked with 2 other engineers in this project, I was in charge of the ratings and reviews portion that allowed users to rate and review products through a form consisting of multiple fields, as well as search through existing reviews.',
   image: { src: shoppingPortal, alt: 'Project-Greenfield' },
   technologies: [
     Technologies.REACT,
@@ -40,7 +40,7 @@ const greenfield = {
 const minesweeperClone = {
   title: 'Minesweeper Clone',
   description:
-    'Minesweeper Clone is a single page web application that mimicks the original Minesweeper that comes with Windows OS. This was the very first web application I built a month a half into coding bootcamp. We were instructed to spend a day on the MVP and see how far we get, but I enjoyed working on it so much I spent a few more days on it.',
+    'Minesweeper Clone is a single page web application that mimicks the original Minesweeper that comes with Windows OS. This was the very first web application I built a month a half into coding bootcamp. We were instructed to spend a day on the MVP in order to see how much we can achieve in a day, but I enjoyed working on the project so much I spent a few more days adding more improvements afterwards.',
   image: { src: minesweeper, alt: 'Minesweeper' },
   technologies: [
     Technologies.REACT,
