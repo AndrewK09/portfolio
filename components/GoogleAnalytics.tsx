8aee79faff220fa6d2783c722b7a3edd9c9f92f8
Andrew <Andrewkn09@Gmail.com>
2023-09-27 09:45:37 -0400

    Rename file

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -3,7 +3,7 @@ import Script from 'next/script';
 
 import { usePathname, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
-import { pageview } from '@/lib/gtagHelper';
+import { pageview } from '@/lib/gtag-helper';
 
 const GoogleAnalytics = ({ GA_TRACKING_ID }: { GA_TRACKING_ID: string }) => {
   const pathname = usePathname();
@@ -18,16 +18,28 @@ const GoogleAnalytics = ({ GA_TRACKING_ID }: { GA_TRACKING_ID: string }) => {
   return (
     <>
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
         strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
       />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-          gtag('config', '${GA_TRACKING_ID}');`}
-      </Script>
+        gtag('consent', 'default', {
+            'analytics_storage': 'denied'
+        });
+        
+        gtag('config', '${GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+        });
+        `,
+        }}
+      />
     </>
   );
 };
