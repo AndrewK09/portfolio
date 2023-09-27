415998e5d0b93faee67bee7d33acbf7757bdcbd4
Andrew <Andrewkn09@Gmail.com>
2023-09-27 09:48:07 -0400

    Wrap google analytics with suspense

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import ActiveSectionContextProvider from '@/context/active-section-context';
 import GoogleAnalytics from '@/components/GoogleAnalytics';
 
 import './globals.css';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: 'Andrew Kan',
@@ -18,9 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <GoogleAnalytics
-        GA_TRACKING_ID={process.env.NEXT_PUBLIC_GA_TRACKING_ID as string}
-      />
+      <Suspense>
+        <GoogleAnalytics
+          GA_TRACKING_ID={process.env.NEXT_PUBLIC_GA_TRACKING_ID as string}
+        />
+      </Suspense>
 
       <body className="bg-primary-navy leading-relaxed text-slate-400 antialiased selection:bg-secondary-green selection:text-secondary-navy">
         <ActiveSectionContextProvider>
