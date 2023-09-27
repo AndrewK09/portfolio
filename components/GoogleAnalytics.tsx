60ec16b1744d116d7a0e10dd4a7fa079932019bd
Andrew <Andrewkn09@Gmail.com>
2023-09-27 09:42:51 -0400

    Add gtagHelper

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -1,7 +1,20 @@
 'use client';
 import Script from 'next/script';
 
+import { usePathname, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+import { pageview } from '@/lib/gtagHelper';
+
 const GoogleAnalytics = ({ GA_TRACKING_ID }: { GA_TRACKING_ID: string }) => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const url = pathname + searchParams.toString();
+
+    pageview(GA_TRACKING_ID, url);
+  }, [pathname, searchParams, GA_TRACKING_ID]);
+
   return (
     <>
       <Script
