f1af392b388dc959f19b2b951427901b2b99468b
Andrew <Andrewkn09@Gmail.com>
2023-09-27 10:34:57 -0400

    Add directly to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import ActiveSectionContextProvider from '@/context/active-section-context';
 import GoogleAnalytics from '@/components/GoogleAnalytics';
 
 import './globals.css';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Andrew Kan',
@@ -22,6 +23,15 @@ export default function RootLayout({
       <GoogleAnalytics
         GA_TRACKING_ID={process.env.NEXT_PUBLIC_GA_TRACKING_ID as string}
       />
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');
+        `}
+      </Script>
       {/* </head> */}
 
       <body className="bg-primary-navy leading-relaxed text-slate-400 antialiased selection:bg-secondary-green selection:text-secondary-navy">
