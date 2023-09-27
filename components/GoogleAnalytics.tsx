784df6b71e3fb267a2637ed6ea0e7f099b16867f
Andrew <Andrewkn09@Gmail.com>
2023-09-27 09:59:19 -0400

    Change to beforeInteractive

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -18,12 +18,12 @@ const GoogleAnalytics = ({ GA_TRACKING_ID }: { GA_TRACKING_ID: string }) => {
   return (
     <>
       <Script
-        strategy="afterInteractive"
+        strategy="beforeInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
       />
       <Script
         id="google-analytics"
-        strategy="afterInteractive"
+        strategy="beforeInteractive"
         dangerouslySetInnerHTML={{
           __html: `
         window.dataLayer = window.dataLayer || [];
