557006526c9afbaa1a714c37c9f1b7ec7931da08
Andrew <Andrewkn09@Gmail.com>
2023-09-27 09:50:45 -0400

    Remove suspense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-'use client';
 import type { Metadata } from 'next';
 import { Toaster } from 'react-hot-toast';
 
@@ -6,7 +5,6 @@ import ActiveSectionContextProvider from '@/context/active-section-context';
 import GoogleAnalytics from '@/components/GoogleAnalytics';
 
 import './globals.css';
-import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: 'Andrew Kan',
@@ -20,11 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <Suspense>
-        <GoogleAnalytics
-          GA_TRACKING_ID={process.env.NEXT_PUBLIC_GA_TRACKING_ID as string}
-        />
-      </Suspense>
+      <GoogleAnalytics
+        GA_TRACKING_ID={process.env.NEXT_PUBLIC_GA_TRACKING_ID as string}
+      />
 
       <body className="bg-primary-navy leading-relaxed text-slate-400 antialiased selection:bg-secondary-green selection:text-secondary-navy">
         <ActiveSectionContextProvider>
