b6219cc98eed56f88becc4d7bea3d062d044c140
Andrew <Andrewkn09@Gmail.com>
2023-09-28 12:57:10 -0400

    Cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,19 +21,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
-      {/* <GoogleAnalytics
-        GA_TRACKING_ID={process.env.NEXT_PUBLIC_GA_TRACKING_ID as string}
-      /> */}
-      {/* <Suspense>
-        <GoogleAnalytics />
-      </Suspense> */}
-      <GoogleTagManager id={process.env.NEXT_PUBLIC_GTM as string} />
+      <head>
+        <GoogleTagManager id={process.env.NEXT_PUBLIC_GTM as string} />
+      </head>
 
-      <noscript
-        dangerouslySetInnerHTML={{
-          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
-        }}
-      />
       <body className="bg-primary-navy leading-relaxed text-slate-400 antialiased selection:bg-secondary-green selection:text-secondary-navy">
         <ActiveSectionContextProvider>
           <Toaster position="top-right" />
