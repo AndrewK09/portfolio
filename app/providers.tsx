4bb36c112e188bae2d9bd57ff4f745d1416bbbc8
Andrew <Andrewkn09@Gmail.com>
2023-09-29 10:16:41 -0400

    Add scroll listerners

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,6 +2,7 @@
 
 import posthog from 'posthog-js';
 import { PostHogProvider } from 'posthog-js/react';
+import { useEffect, useRef } from 'react';
 
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
@@ -10,6 +11,51 @@ if (typeof window !== 'undefined') {
 }
 
 const PHProvider = ({ children }: { children: React.ReactNode }) => {
+  const maxPercentage = useRef(0);
+  const maxPixels = useRef(0);
+  useEffect(() => {
+    function handleScroll() {
+      const lastPercentage = Math.min(
+        1,
+        (window.innerHeight + window.pageYOffset) / document.body.offsetHeight
+      );
+      const lastPixels = window.innerHeight + window.pageYOffset;
+      if (lastPercentage > maxPercentage.current) {
+        maxPercentage.current = lastPercentage;
+      }
+      if (lastPixels > maxPixels.current) {
+        maxPixels.current = lastPixels;
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handlePageleave = () => {
+      posthog.capture('$pageleave', {
+        'max scroll percentage': maxPercentage.current,
+        'max scroll pixels': maxPixels.current,
+        'last scroll percentage': Math.min(
+          1,
+          (window.innerHeight + window.pageYOffset) / document.body.offsetHeight
+        ),
+        'last scroll pixels': window.innerHeight + window.pageYOffset,
+        scrolled: maxPixels.current > 0,
+      });
+    };
+
+    window.addEventListener('beforeunload', handlePageleave);
+
+    return () => {
+      window.removeEventListener('beforeunload', handlePageleave);
+    };
+  }, []);
+
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 };
 
